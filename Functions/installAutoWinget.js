bbe29428530b31c35497527deeee2e36d3d033d3
Sachanime <escalierquitue@gmail.com>
2026-05-18 23:01:55 +0200

    Continue installAutoWinget

--- Functions/installAutoWinget.js
+++ Functions/installAutoWinget.js
@@ -1,4 +1,6 @@
+const { exec } = require('child_process')
 const { readRegistry } = require('./readRegistry')
+const { createFolder } = require('./createFolder')
 
 async function installAutoWinget() {
 
@@ -6,9 +8,44 @@ async function installAutoWinget() {
     console.log("Startin Auto-Winget installation...")
     console.log("Checking your Windows version...")
 
+    const requiredBuild = 26100
     const windowsBuild = await readRegistry('HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion', 'CurrentBuild')
+    const buildNumber = parseInt(windowsBuild, 10)
 
-    console.log(windowsBuild)
+    console.log("Your Windows version is 10.0." + buildNumber)
+
+    if(buildNumber > requiredBuild) {
+        console.log("Your system is ready")
+    }
+
+    else {
+        console.error("Your system is not compatible")
+        return
+    }
+
+    console.log("Activating sudo...")
+
+    const command = 'sudo config --enable normal'
+
+    exec(command, (error, stdout, stderr) => {
+
+        if(error) {
+            console.error('Execution error :\n', error.message)
+            return
+        }
+
+        if(stderr) {
+            console.error('System error :\n', stderr)
+            return
+        }
+
+        console.log(stdout || 'Sudo activated')
+
+    })
+
+    console.log("Creating directories...")
+
+    createFolder('C:\\Program Files\\SKL\\Auto-Winget')
 
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const { init, displayMenu } = require('./Functions/index')
+const { init, displayMenu, installAutoWinget } = require('./Functions/index')
 
 async function main() {
 
     await init()
     const choice = await displayMenu()
+
+    if(choice == 1) {
+        await installAutoWinget()
+    }
 
 }
 
