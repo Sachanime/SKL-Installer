3824930c5de32b7d3089a72a3c657083f7b6aea7
Sachanime <escalierquitue@gmail.com>
2026-05-18 15:54:49 +0200

    Begin installAutoWinget

---
+++ Functions/installAutoWinget.js
@@ -0,0 +1,9 @@
+const regedit = require('regedit').promisified
+
+async function installAutoWinget() {
+
+    process.stdout.write('\x1B[2J\x1B[3J\x1B[H')
+    console.log("Startin Auto-Winget installation...")
+    console.log("Checking your Windows version...")
+
+}
