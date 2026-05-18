65f14080b2a00c37439295d15264a355a98f2367
Sachanime <escalierquitue@gmail.com>
2026-05-18 22:24:41 +0200

    Log Windows build number

--- Functions/installAutoWinget.js
+++ Functions/installAutoWinget.js
@@ -1,7 +1,13 @@
+const { readRegistry } = require('./readRegistry')
+
 async function installAutoWinget() {
 
     process.stdout.write('\x1B[2J\x1B[3J\x1B[H')
     console.log("Startin Auto-Winget installation...")
     console.log("Checking your Windows version...")
+
+    const windowsBuild = await readRegistry('HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion', 'CurrentBuild')
+
+    console.log(windowsBuild)
 
 }
