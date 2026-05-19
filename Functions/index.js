6ddb71bfa6c139e8bf82a1783fa806d89792c4a9
Sachanime <escalierquitue@gmail.com>
2026-05-19 19:34:47 +0200

    Index function

--- Functions/index.js
+++ Functions/index.js
@@ -3,5 +3,6 @@ const { createFolder } = require('./createFolder')
 const { curl } = require('./curl')
 const { displayBanner } = require('./displayBanner')
 const { readRegistry } = require('./readRegistry')
+const { importScheduledTask } = require('./importScheduledTask')
 
-module.exports = { displayMenu, createFolder, curl, displayBanner, readRegistry }
+module.exports = { displayMenu, createFolder, curl, displayBanner, readRegistry, importScheduledTask }
