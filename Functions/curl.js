266de81e7fb6832f36835523dfd9dbf48967fd37
Sachanime <escalierquitue@gmail.com>
2026-05-19 13:45:46 +0200

    Create curl.js

---
+++ Functions/curl.js
@@ -0,0 +1,23 @@
+const { exec } = require('child_process')
+
+async function curl(url, path) {
+
+    const command = 'curl -L ' + url + ' -o ' + path
+
+    exec(command, (error, stdout, stderr) => {
+
+        if(error) {
+            console.error("Execution error :\n", error)
+            return
+        }
+
+        if(stderr) {
+            console.error("System error :\n", stderr)
+            return
+        }
+
+        console.log(stdout || "File downloaded")
+
+    })
+
+}
