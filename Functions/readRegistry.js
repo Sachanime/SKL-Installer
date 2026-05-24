77715481aad92859f506e35ec3e938011a7f3188
Sachanime <escalierquitue@gmail.com>
2026-05-24 19:45:25 +0200

    Fix some bugs

--- Functions/readRegistry.js
+++ Functions/readRegistry.js
@@ -1,23 +1,24 @@
-const regedit = require('regedit').promisified
+const regeditRaw = require('regedit')
+const regedit = regeditRaw.promisified
 const path = require('path')
 const { extractFiles } = require('./extractFiles')
 
 async function readRegistry(regPath, regValue) {
 
     const isPkg = typeof process.pkg !== 'undefined'
-    const vbsTargetDir = path.join(process.env.TEMP, skl, vbs)
-    const vbsSource = path.join('..', 'node_modules', 'regedit', 'vbs' )
-    const vbsFiles = ['regList.wsf', 'regRead.wsf', 'regWrite.wsf', 'regQuery.wsf', 'regDelete.wsf']
+    const vbsTargetDir = path.join(process.env.TEMP, 'skl', 'vbs')
+    const vbsSource = path.resolve(process.cwd(), 'node_modules', 'regedit', 'vbs' )
+    const vbsFiles = ['ArchitectureAgnosticRegistry.vbs', 'ArchitectureSpecificRegistry.vbs', 'JsonSafeTest.wsf', 'regCreateKey.wsf', 'regDeleteKey.wsf', 'regDeleteValue.wsf', 'regList.wsf', 'regListStream.wsf', 'regPutValue.wsf', 'regUtil.vbs', 'util.vbs', 'wsRegReadList.wsf', 'wsRegReadListStream.wsf']
 
     if(isPkg) {
 
         try {
             await extractFiles(vbsSource, vbsTargetDir, vbsFiles)
-            regedit.setExternealVbsDir(vbsTargetDir)
+            regeditRaw.setExternalVBSLocation(vbsTargetDir)
         }
 
         catch(err) {
-            console.error('Extraction failed')
+            console.error('Extraction failed', err)
             throw(err)
         }
 
